1c2821f6ae2adf22889c4b0501f2247dbe67b331
Soliloquys <simonziggy18@gmail.com>
2023-08-31 18:49:41 -0700

    correction

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
- * _puts_recursion -function like puts() in stdlib
- * @s; input
+ * _puts_recursion - function like puts();
+ * @s: input
  * Return: Always 0 (Success)
  */
 void _puts_recursion(char *s)
 {
-	if(*s!=0)
+	if (*s)
 	{
-		_putchar(*s);
+		_putchar(*s != 0);
 		_puts_recursion(++s);
 	}
 
 	else
 		_putchar('\n');
 }
+
