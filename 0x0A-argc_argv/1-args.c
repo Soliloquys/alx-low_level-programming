0ee792dde0dcdc4a5aa53f379128e5d4e2223cfa
Soliloquys <simonziggy18@gmail.com>
2023-09-06 16:10:24 -0700

    task 2

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,8 +9,9 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char *argv[]__attribute__((unused)))
+int main(int argc, char *argv[])
 {
+(void) argv; /*unused param bypass*/
         printf("%d\n", argc - 1);
 
         return (0);
