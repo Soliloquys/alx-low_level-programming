447a175675e09376d6b9369873b7eeb0f9271ad2
Soliloquys <simonziggy18@gmail.com>
2023-09-12 13:14:32 -0700

    filename

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This function prints the name of the current source file.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
