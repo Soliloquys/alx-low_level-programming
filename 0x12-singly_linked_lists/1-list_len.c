169f29eba96cb5b649c22c730c0703fac5e498c2
Soliloquys <simonziggy18@gmail.com>
2023-09-23 15:29:08 +0000

    listlen

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,21 +3,19 @@
 #include "lists.h"
 
 /**
- *  * print_list - prints the element of a singly linked list
- *   *
- *    * @h: pointer to head of the linked list
- *     *
- *      * Return: number of nodes in the list
- *       */
+ * print_list - prints the element of a singly linked list
+ * @h: pointer to head of the linked list
+ *
+ * Return: number of nodes in the list
+ */
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
-	
+
 	while (h != NULL)
 	{
 		h = h->next;
-		count++;	
+		count++;
 	}
-       
 	return (count);
 }
