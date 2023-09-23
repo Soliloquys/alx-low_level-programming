a105920d5da082cd020ade783945f10ad198a6e4
Soliloquys <simonziggy18@gmail.com>
2023-09-23 15:17:29 +0000

    listlen

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *  * print_list - prints the element of a singly linked list
+ *   *
+ *    * @h: pointer to head of the linked list
+ *     *
+ *      * Return: number of nodes in the list
+ *       */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;	
+	}
+       
+	return (count);
+}
