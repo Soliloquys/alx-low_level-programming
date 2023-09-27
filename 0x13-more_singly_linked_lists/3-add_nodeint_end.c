f37fea87fa5317dfd8746e2a408f621c21b83ada
Soliloquys <simonziggy18@gmail.com>
2023-09-27 01:10:40 +0000

    free

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,24 +1,34 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * add_nodeint_end - adds a node at thhe end of a list
+ * @head: pointer to the first element in the list
+ * @n: data inserted into the first element
+ *
+ * Return: pointer to the new node or NULL if it fails
+ */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
-	listint_t* last = *head;
+	listint_t *last = *head;
+
+	if (new_node == NULL)
+		return (NULL);
 
 	new_node->n = n;
 
 	new_node->next = NULL;
+
 	if (*head == NULL)
 	{
 		*head = new_node;
-		return (NULL);
+		return (new_node);
 	}
 	while (last->next != NULL)
 	{
 		last = last->next;
 	}
 	last->next = new_node;
-	
-	return new_node;
+	return (new_node);
 }
---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: listint_t list to be freed
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *new;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = new;
+	}
+}
+
